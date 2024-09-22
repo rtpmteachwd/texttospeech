be9764948ce05afe5ca2008c5dbcf320275f036a
rtpmteachwd <brazcimafranca@gmail.com>
2024-09-22 11:47:52 +0800

    edited the app.js

--- app.js
+++ app.js
@@ -7,24 +7,28 @@ if ("webkitSpeechRecognition" in window) {
   recognition.interimResults = false;
   recognition.lang = "en-US";
 
-  startRecognitionBtn.addEventListener("click", function () {
+  startRecognitionBtn.addEventListener("click", () => {
     recognition.start();
-    transcription.innerHTML = "Listening";
+    transcription.innerHTML = "Listening...";
   });
 
   recognition.onresult = function (event) {
     const transcript = event.results[0][0].transcript;
-    transcript.innerHTML = `You said: ${transcript}}`;
+    transcription.innerHTML = `You said: ${transcript}`;
+  };
+
+  recognition.onerror = function (event) {
+    transcription.innerHTML = "Error occurred in recognition: " + event.error;
   };
 } else {
   transcription.innerHTML =
-    "Speech recognition API  is not supported on this browser";
+    "Speech Recognition API not supported in this browser.";
 }
 
 const speakTextBtn = document.getElementById("speak-text");
 const textToSpeak = document.getElementById("text-to-speak");
 
-speakTextBtn.addEventListener("click", function () {
+speakTextBtn.addEventListener("click", () => {
   const text = textToSpeak.value;
   if (text.trim() !== "") {
     const utterance = new SpeechSynthesisUtterance(text);
